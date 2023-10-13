e428f499c67c85f5bfa2510a1cba5f1e8e7fc581
Ilkay Yanar <ilkayyanar@Ilkays-MacBook-Pro.local>
2023-10-13 18:32:29 +0200

    modification

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,38 +6,41 @@
 /*   By: ilyanar <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 14:14:27 by ilyanar           #+#    #+#             */
-/*   Updated: 2023/10/12 16:55:04 by ilyanar          ###   ########.fr       */
+/*   Updated: 2023/10/13 17:27:10 by ilyanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
+	size_t	k;
 
-	i = ft_strlen(src);
+	i = ft_strlen(dst);
+	k = i;
 	j = 0;
-	while ((i + j <= dstsize - 1) || ( src[j] == '\0'))
+	while (((i + j) <= dstsize - 2) && (src[j] != '\0'))
 	{
 		dst[i + j] = src[j];
 		j++;
 	}
 	dst[i + j] = '\0';
-	return (ft_strlen(dst));
+	return (k + ft_strlen(src));
 }
-
+/*
 #include <stdio.h>
 #include <string.h>
-int	main()
+int	main(void)
 {
-	char dst[20] = "Hello ";
-	char src[] = "Worlddawsdawsdawdsawdawd!";
+	char dst[15] = "Hello ";
+	char src[] = "wdbhawduawvdbaw,bd,kahwbd,bawldawdorld";
 
-	printf("%zu\n", ft_strlcat(dst, src, 20));
-	printf("%s", dst);
-	//printf("%zu\n", strlcat(dst, src, 20));
-	//printf("%s", dst);
+	printf("- Mon test : %zu\n", ft_strlcat(dst, src, 15));
+	printf("%s\n", dst);
+	//printf("- Vrais fonction : %zu\n", strlcat(dst, src, 15));
+	//printf("%s\n", dst);
 }
+*/
