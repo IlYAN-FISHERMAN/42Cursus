54123b26060ed7bbb94aa229cb767dc5dcdbdd0a
Ilkay Yanar <ilkayyanar@Ilkays-MacBook-Pro.local>
2023-10-14 18:36:17 +0200

    lib a jour

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,33 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ilyanar <ilyanar@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/14 17:10:43 by ilyanar           #+#    #+#             */
-/*   Updated: 2023/10/14 17:38:23 by ilyanar          ###   ########.fr       */
+/*   Created: 2023/10/14 17:39:22 by ilyanar           #+#    #+#             */
+/*   Updated: 2023/10/14 18:14:17 by ilyanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	while (*s1 == *s2 && n - 1 > 0)
+	while ((*(const char *)s != (unsigned char)c) && (n - 1 > 0))
 	{
-		s1++;
-		s2++;
+		s++;
 		n--;
 	}
-	return ((int)*s1 - (int)*s2);
+	if (*(char *)s == '\0')
+		return (0);
+	return ((void *)s);
 }
 /*
 #include <stdio.h>
 int	main()
 {
-	char tab1[] = "hellPD";
-	char tab2[] = "hellP";
-	printf("%d", ft_strncmp(tab1, tab2, 10));
+	char tab[] = "gello uys";
+	char c = 'g';
+
+	printf("%s", (char *)ft_memchr(tab, c, 10));
 }
 */
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ilyanar <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 17:33:48 by ilyanar           #+#    #+#             */
-/*   Updated: 2023/10/12 14:16:25 by ilyanar          ###   ########.fr       */
+/*   Updated: 2023/10/14 18:35:05 by ilyanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,24 @@
 
 # include <stdlib.h>
 
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 
 #endif
