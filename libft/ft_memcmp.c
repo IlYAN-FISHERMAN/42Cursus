094e877e1f78081b440debd35b73b7eef13cd4d7
Ilkay Yanar <ilkayyanar@Ilkays-MacBook-Pro.local>
2023-10-17 11:10:27 +0200

    pc maison

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -1,33 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ilyanar <ilyanar@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/14 17:10:43 by ilyanar           #+#    #+#             */
-/*   Updated: 2023/10/14 17:38:23 by ilyanar          ###   ########.fr       */
+/*   Created: 2023/10/14 20:52:29 by ilyanar           #+#    #+#             */
+/*   Updated: 2023/10/16 15:51:04 by ilyanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	while (*s1 == *s2 && n - 1 > 0)
+	while (*(const char *)s1 == *(const char *)s2 && n > 0)
 	{
 		s1++;
 		s2++;
 		n--;
 	}
-	return ((int)*s1 - (int)*s2);
+	return (*(int *)s1 - *(int *)s2);
 }
-
+/*
 #include <stdio.h>
 int	main()
 {
-	char tab1[] = "hellPD";
-	char tab2[] = "hellP";
-	printf("%d", ft_strncmp(tab1, tab2, 3));
+	char tab[] = "hello";
+	char tab2[] = "hello";
+	printf("%d" , ft_memcmp(tab, tab2, 3));
 }
-
+*/
