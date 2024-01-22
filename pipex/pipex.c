6fad3a59bf71fb8371d0f3e5f172f12660d8848f
Ilkay Yanar <ilkayyanar@Ilkays-MacBook-Pro.local>
2024-01-22 13:53:51 +0100

    update from home

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ilyanar <ilyanar@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:03:25 by ilyanar           #+#    #+#             */
-/*   Updated: 2024/01/19 03:12:48 by ilyanar          ###   ########.fr       */
+/*   Updated: 2024/01/20 17:56:53 by ilyanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	main(int ac, char **av)
 		perror("\033[31mError\033[0m");
 		exit(EXIT_FAILURE);
 	}
-	else
+	else if (access(av[1], R_OK) == 0)
 	{
 		fd = open(av[1], O_RDONLY);
 		tab = ft_calloc(101, sizeof(char));
