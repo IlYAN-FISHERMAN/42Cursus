560967997c5ceee43be72afd455cb1643b0e645e
Ilkay Yanar <ilyanar@c3r1s1.42lausanne.ch>
2024-01-22 22:34:57 +0100

    update from school pipex

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,30 +6,46 @@
 /*   By: ilyanar <ilyanar@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:03:25 by ilyanar           #+#    #+#             */
-/*   Updated: 2024/01/20 17:56:53 by ilyanar          ###   ########.fr       */
+/*   Updated: 2024/01/22 16:56:39 by ilyanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/fcntl.h>
+#include <sys/unistd.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int	main(int ac, char **av)
+int	main(int ac, char **av, char *envp[])
 {
-	char	*tab;
-	int		fd;
+	int		pid;
+	char	*arg[] = {av[2], NULL, NULL};
+	char	**tab;
 
 	tab = 0;
-	if (ac != 5)
+	if (ac < 1)
+		exit(EXIT_FAILURE);
+	pid = fork();
+	if (pid == -1)
 	{
 		perror("\033[31mError\033[0m");
 		exit(EXIT_FAILURE);
 	}
-	else if (access(av[1], R_OK) == 0)
+	else if (pid == 0)
 	{
-		fd = open(av[1], O_RDONLY);
-		tab = ft_calloc(101, sizeof(char));
-		read(fd, tab, 100);
-		ft_printf("%s\n", tab);
-		close(fd);
+		tab = ft_split(envp[15], ':');
+		if (access(av[2], F_OK) != -1)
+			execve("/bin/ls", arg, NULL);
+		perror("\033[31mError\033[0m");
+		exit(EXIT_FAILURE);
 	}
+	else if (waitpid(pid, NULL, 0))
+	{
+		open("fdp", O_CREAT);
+		ft_printf("end\n");
+	}
+	ft_free_tab(tab);
 	return (0);
 }
