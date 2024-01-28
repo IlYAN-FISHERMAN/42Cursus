88f179517381219a56ce0d19495906f763816617
Ilkay Yanar <ilkayyanar@Ilkays-MacBook-Pro.local>
2024-01-28 18:22:57 +0100

    update

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,13 +6,14 @@
 /*   By: ilyanar <ilyanar@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:03:25 by ilyanar           #+#    #+#             */
-/*   Updated: 2024/01/26 18:58:54 by ilyanar          ###   ########.fr       */
+/*   Updated: 2024/01/26 19:53:46 by ilyanar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/fcntl.h>
 #include <sys/unistd.h>
 #include <sys/wait.h>
@@ -42,13 +43,10 @@ int	check_path(char **tab, char **env)
 {
 	char	*path;
 	int		i;
-	char tabl[] = "hello";
 
 	i = -1;
 	path = path_line(env, "PATH");
 	tab = ft_split(path, ':');
-	free(tab[0]);
-	tab[0] = tabl;
 	if (tab)
 		while (tab[++i])
 			ft_printf("\033[38;5;208m%s\033[0m\n", tab[i]);
@@ -57,14 +55,25 @@ int	check_path(char **tab, char **env)
 	return (1);
 }
 
+int	check_command(char **tab, char *arg)
+{
+}
+
 int	pipex(char **av, char **envp, int ac)
 {
+	int		i;
 	int		pid;
 	char	**tab;
 	char	**arg;
 
+	i = 0;
 	tab = 0;
 	arg = 0;
+	if (check_path(tab, envp) == 0)
+	{
+		perror("\033[31mError path\033[0m");
+		exit(EXIT_FAILURE);
+	}
 	pid = fork();
 	if (pid == -1)
 	{
@@ -73,17 +82,7 @@ int	pipex(char **av, char **envp, int ac)
 	}
 	if (pid == 0)
 	{
-		if (access(av[1], R_OK) != 0)
-		{
-			perror("\033[31mError access\033[0m");
-			exit(EXIT_FAILURE);
-		}
-		if (check_path(tab, envp) == 0)
-		{
-			perror("\033[31mError path\033[0m");
-			exit(EXIT_FAILURE);
-		}
-		execve("/bin/ls", arg, NULL);
+		execve("/bin/ls", arg, envp);
 		perror("\033[31mError child\033[0m");
 		exit(EXIT_FAILURE);
 	}
@@ -92,6 +91,7 @@ int	pipex(char **av, char **envp, int ac)
 		open(av[ac], O_CREAT | O_TRUNC | O_WRONLY, 0644);
 		ft_printf("end\n");
 	}
+	ft_free_tab(tab);
 	return (0);
 }
 
@@ -99,7 +99,7 @@ int	main(int ac, char **av, char **envp)
 {
 	if (((ac < 5) || (access(av[1], R_OK) != 0)))
 	{
-		perror("Error");
+		strerror(-1);
 		exit(EXIT_FAILURE);
 	}
 	else
